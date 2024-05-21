f03a6fd0895cf640f8ab1d8f1a59141c92ac9f1b
HongyiXu <96808394+LukaHongyi@users.noreply.github.com>
2024-05-21 10:40:05 +0800

    Update transfer_files.py

--- utl/transfer_files.py
+++ utl/transfer_files.py
@@ -3,13 +3,13 @@ import paramiko
 
 def transfer_file(local_path, des_path):
     # 实例化一个trans对象# 实例化一个transport对象
-    trans = paramiko.Transport(('58.34.161.11', 22220))
+    trans = paramiko.Transport(('xxxxxxx', xxxxx))
     # 建立连接
-    trans.connect(username='xhy', password='123456')
+    trans.connect(username='xxxxx', password='xxxxxx')
 
     # 实例化一个 sftp对象,指定连接的通道
     sftp = paramiko.SFTPClient.from_transport(trans)
     sftp.put(localpath=local_path, remotepath=des_path)
     trans.close()
 
-transfer_file("./output/json_dataset/10.3390_app13158876.json", "/home/xhy/json_dataset/10.3390_app13158876.json")
+transfer_file("./output/json_dataset/10.3390_app13158876.json", "/home/xxxx/json_dataset/10.3390_app13158876.json")
